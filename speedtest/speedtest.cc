79e4be4905cefd46e014a03fa220868b5a933e65
Timo Bingmann <tbgit@panthema.net>
2008-08-12 08:30:17 +0000

    Changing speed test code: modularizing it to run both speed tests (insert-only and all-ops) in one program. Also speedtesting until at least 1.0s time elapsed.

--- speedtest/speedtest.cc
+++ speedtest/speedtest.cc
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 
@@ -39,17 +40,11 @@ const unsigned int mininsertnum = 125;
 /// maximum number of items to insert
 const unsigned int maxinsertnum = 1024000 * 4;
 
-/// repeat each test until this number of items was inserted in total (loop
-/// over very short tests)
-const unsigned int repeatinsertuntil = maxinsertnum * 2;
-
 const int randseed = 34234235;
 
+/// b+ tree slot range to test
 const int min_nodeslots = 4;
 const int max_nodeslots = 256;
-
-const bool with_find = false;
-const bool with_erase = false;
 
 /// Time is measured using gettimeofday()
 inline double timestamp()
@@ -70,117 +65,189 @@ struct btree_traits_speed
     static const int    innerslots = _leafslots;
 };
 
-/// Test the multiset red-black tree from STL
-void test_set(unsigned int insertnum)
+/// Test the multiset red-black tree from STL (only insert)
+struct test_set_insert
 {
-    typedef std::multiset<unsigned int> multiset_type;
-    multiset_type set;
-
-    srand(randseed);
-    for(unsigned int i = 0; i < insertnum; i++)
-        set.insert( rand() );
-
-    assert( set.size() == insertnum );
-
-    if (with_find)
+    void operator()(unsigned int insertnum)
     {
-        srand(randseed);
-        for(unsigned int i = 0; i < insertnum; i++)
-            set.find(rand());
+	typedef std::multiset<unsigned int> multiset_type;
+	multiset_type set;
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    set.insert( rand() );
+
+	assert( set.size() == insertnum );
     }
+};
 
-    if (with_erase)
-    {
-        srand(randseed);
-        for(unsigned int i = 0; i < insertnum; i++)
-            set.erase( set.find(rand()) );
-
-        assert( set.empty() );
-    }
-}
-
-/// Test the B+ tree with a specific leaf/inner slots
+/// Test the B+ tree with a specific leaf/inner slots (only insert)
 template <int _slots>
-void test_btree(unsigned int insertnum)
+struct test_btree_insert
 {
-    typedef stx::btree_multiset<unsigned int, std::less<unsigned int>,
-        struct btree_traits_speed<_slots, _slots> > btree_type;
-
-    btree_type bt;
-
-    srand(randseed);
-    for(unsigned int i = 0; i < insertnum; i++)
-        bt.insert(rand());
-
-    assert( bt.size() == insertnum );
-
-    if (with_find)
+    void operator()(unsigned int insertnum)
     {
-        srand(randseed);
-        for(unsigned int i = 0; i < insertnum; i++)
-            bt.exists(rand());
-    }
+	typedef stx::btree_multiset<unsigned int, std::less<unsigned int>,
+	    struct btree_traits_speed<_slots, _slots> > btree_type;
 
-    if (with_erase)
+	btree_type bt;
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    bt.insert(rand());
+
+	assert( bt.size() == insertnum );
+    }
+};
+
+/// Test the multiset red-black tree from STL (insert, find and delete)
+struct test_set_insert_find_delete
+{
+    void operator()(unsigned int insertnum)
     {
-        srand(randseed);
-        for(unsigned int i = 0; i < insertnum; i++)
-            bt.erase_one(rand());
+	typedef std::multiset<unsigned int> multiset_type;
+	multiset_type set;
 
-        assert(bt.empty());
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    set.insert( rand() );
+
+	assert( set.size() == insertnum );
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    set.find(rand());
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    set.erase( set.find(rand()) );
+
+	assert( set.empty() );
     }
-}
+};
+
+/// Test the B+ tree with a specific leaf/inner slots (insert, find and delete)
+template <int Slots>
+struct test_btree_insert_find_delete
+{
+    void operator()(unsigned int insertnum)
+    {
+	typedef stx::btree_multiset<unsigned int, std::less<unsigned int>,
+	    struct btree_traits_speed<Slots, Slots> > btree_type;
+
+	btree_type bt;
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    bt.insert(rand());
+
+	assert( bt.size() == insertnum );
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    bt.exists(rand());
+
+	srand(randseed);
+	for(unsigned int i = 0; i < insertnum; i++)
+	    bt.erase_one(rand());
+
+	assert(bt.empty());
+    }
+};
+
+unsigned int repeatuntil;
 
 /// Repeat (short) tests until enough time elapsed and divide by the runs.
-template <void (*func)(unsigned int)>
-void testrunner_loop(unsigned int insertnum)
+template <typename functional>
+void testrunner_loop(std::ostream& os, unsigned int insertnum)
 {
     unsigned int runs = 0;
+    double ts1, ts2;
 
-    double ts1 = timestamp();
-
-    for(unsigned int totaltests = 0; totaltests <= repeatinsertuntil; totaltests += insertnum)
+    do
     {
-        func(insertnum);
-        ++runs;
+	runs = 0;
+
+	ts1 = timestamp();
+
+	for(unsigned int totaltests = 0; totaltests <= repeatuntil; totaltests += insertnum)
+	{
+	    functional()(insertnum);
+	    ++runs;
+	}
+
+	ts2 = timestamp();
+
+	std::cerr << "Insert " << insertnum << " repeat " << (repeatuntil / insertnum) << " time " << (ts2 - ts1) << "\n";
+
+	if ((ts2 - ts1) < 1.0) repeatuntil *= 2;
     }
+    while ((ts2 - ts1) < 1.0);
 
-    double ts2 = timestamp();
-
-    std::cout << std::fixed << std::setprecision(10) << ((ts2 - ts1) / runs) << " " << std::flush;
+    os << std::fixed << std::setprecision(10) << ((ts2 - ts1) / runs) << " " << std::flush;
 }
 
 // Template magic to emulate a for_each slots. These templates will roll-out
 // btree instantiations for each of the Low-High leaf/inner slot numbers.
-template<int Low, int High>
+template< template<int Slots> class functional, int Low, int High>
 struct btree_range
 {
-    inline void operator()(unsigned int insertnum)
+    inline void operator()(std::ostream& os, unsigned int insertnum)
     {
-        testrunner_loop< test_btree<Low> >(insertnum);
-        btree_range<Low+1, High>()(insertnum);
+        testrunner_loop< functional<Low> >(os, insertnum);
+        btree_range<functional, Low+1, High>()(os, insertnum);
     }
 };
 
-template<int Low>
-struct btree_range<Low, Low>
+template< template<int Slots> class functional, int Low>
+struct btree_range<functional, Low, Low>
 {
-    inline void operator()(unsigned int insertnum) {
-        testrunner_loop< test_btree<Low> >(insertnum);
+    inline void operator()(std::ostream& os, unsigned int insertnum)
+    {
+        testrunner_loop< functional<Low> >(os, insertnum);
     }
 };
 
 /// Speed test it!
 int main()
 {
-    for(unsigned int insertnum = mininsertnum; insertnum <= maxinsertnum; insertnum *= 2)
-    {
-        std::cout << insertnum << " " << std::flush;
+    { // speed test only insertion
 
-        testrunner_loop<test_set>(insertnum);
+	std::ofstream os("speed-insert.txt");
 
-        btree_range<min_nodeslots, max_nodeslots>()(insertnum);
+	repeatuntil = mininsertnum;
 
-        std::cout << "\n" << std::flush;
+	for(unsigned int insertnum = mininsertnum; insertnum <= maxinsertnum; insertnum *= 2)
+	{
+	    std::cerr << "Insert " << insertnum << "\n";
+
+	    os << insertnum << " " << std::flush;
+
+	    testrunner_loop<test_set_insert>(os, insertnum);
+
+	    btree_range<test_btree_insert, min_nodeslots, max_nodeslots>()(os, insertnum);
+
+	    os << "\n" << std::flush;
+	}
+    }
+
+    { // speed test insert, find and delete
+
+	std::ofstream os("speed-all.txt");
+	
+	repeatuntil = mininsertnum;
+
+	for(unsigned int insertnum = mininsertnum; insertnum <= maxinsertnum; insertnum *= 2)
+	{
+	    std::cerr << "Insert, Find, Delete " << insertnum << "\n";
+
+	    os << insertnum << " " << std::flush;
+
+	    testrunner_loop<test_set_insert_find_delete>(os, insertnum);
+
+	    btree_range<test_btree_insert_find_delete, min_nodeslots, max_nodeslots>()(os, insertnum);
+
+	    os << "\n" << std::flush;
+	}
     }
 }
