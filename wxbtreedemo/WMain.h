0b301a4f281fd171db40eb43232a4613ef696862
Timo Bingmann <tbgit@panthema.net>
2013-05-05 21:51:45 +0200

    Relicensing the template source code under the Boost license and the rest under GPLv3.

--- wxbtreedemo/WMain.h
+++ wxbtreedemo/WMain.h
@@ -1,10 +1,10 @@
 /*
- * STX B+ Tree Demo Program v0.8.6
+ * STX B+ Tree Demo Program v0.9
  * Copyright (C) 2008-2013 Timo Bingmann
  *
- * This program is free software; you can redistribute it and/or modify it
+ * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
+ * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
@@ -13,8 +13,7 @@
  * more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _WMain_H_
